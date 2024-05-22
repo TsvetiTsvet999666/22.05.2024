1b780a4b8fc79438c40e04af0c5838c3941e54c3
TsvetiTsvet999666 <159997613+TsvetiTsvet999666@users.noreply.github.com>
2024-05-22 14:54:01 +0300

    22.05.2024

---
+++ ImprovedPhoneBook/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ImprovedPhoneBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new SortedDictionary<string, string>();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "END")
+                {
+                    break;
+                }
+                string[] parts = command.Split(' ');
+                string action = parts[0];
+                if (action == "A")
+                {
+                    string name = parts[1];
+                    string phoneNumber = parts[2];
+                    phoneBook[name] = phoneNumber; 
+                }
+                else if (action == "S")
+                {
+                    string name = parts[1];
+                    if (phoneBook.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {phoneBook[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+                else if (action == "ListAll")
+                {
+                    Console.WriteLine($"В указателя има {phoneBook.Count} записани.");
+                    Console.WriteLine("Списък по азбучен ред:");
+                    foreach (var entry in phoneBook)
+                    {
+                        Console.WriteLine($"{entry.Key} -> {entry.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NumberOfRealNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace NumberOfRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a list of real numbers separated by spaces:");
+            string input = Console.ReadLine();
+            string[] parts = input.Split( );
+            var counts = new SortedDictionary<double, int>();
+            foreach (string part in parts)
+            {
+                double num = double.Parse(part);
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+            }
+            foreach (var num in counts.Keys)
+            {
+                Console.WriteLine($"{num} -> {counts[num]}");
+            }
+
+        }
+    }
+}
